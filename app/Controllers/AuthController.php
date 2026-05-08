9b8dfd6815220c61bfb9823813d4c3e5e46f8b38
aqiss <balqisdamia99@gmail.com>
2026-05-08 09:41:16 +0700

    fix: perbaiki typo komentar di AuthController

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -18,7 +18,7 @@ class AuthController extends BaseController
             $username = $this->request->getVar('username');
             $password = $this->request->getVar('password');
 
-            $dataUser = ['username' => 'Damia', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
+            $dataUser = ['username' => 'Damia', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // password 123
 
             if ($username == $dataUser['username']) {
                 if (md5($password) == $dataUser['password']) {
